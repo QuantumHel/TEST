e35304f897711ce1273d5cbd51ca2278fa43018c
Le0nerdo <leo.becker@helsinki.fi>
2026-02-02 22:38:06 +0200

    work on more drawing functionality

--- examples/draw/main.rs
+++ examples/draw/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::Write;
-
+use test_transpiler::draw::{VisualRow, VisualText, draw_rows};
 use test_transpiler::{
 	draw::ImageSize,
 	pauli::{PauliAngle, PauliExp},
@@ -8,6 +8,9 @@ use test_transpiler::{
 };
 
 fn main() {
+	let size = ImageSize::FixedWidth(500);
+
+	/*
 	let p: PauliExp<PauliAngle> = PauliExp {
 		angle: PauliAngle::MultipleOfPi(1.4),
 		string: pauli_string!("XYZIXXY"),
@@ -18,9 +21,38 @@ fn main() {
 		pauli_string!("ZXXXIII"),
 		pauli_string!("XZZXIII"),
 	];
-	let size = ImageSize::FixedWidth(500);
 
 	let svg = p.draw_pi_over_4_evolution(&strings, size);
+	 */
+
+	let rows = vec![
+		VisualRow::String {
+			name: VisualText::plain_text("A"),
+			letters: vec![
+				Some(VisualText::plain_text("P").with_superscript("b")),
+				Some(
+					VisualText::plain_text("P")
+						.with_subscript("a")
+						.with_superscript("++"),
+				),
+				None,
+				Some(VisualText::plain_text("A")),
+			],
+		},
+		VisualRow::Arrow,
+		VisualRow::Empty,
+		VisualRow::String {
+			name: VisualText::plain_text("A"),
+			letters: vec![
+				Some(VisualText::plain_text("A")),
+				Some(VisualText::plain_text("A")),
+				None,
+				Some(VisualText::plain_text("A")),
+			],
+		},
+	];
+
+	let svg = draw_rows(rows, size);
 
 	let mut file = File::options()
 		.write(true)
--- src/draw.rs
+++ src/draw.rs
@@ -193,3 +193,198 @@ impl<T: Negate + Clone> PauliExp<T> {
 		res
 	}
 }
+
+pub struct VisualText {
+	pub text: String,
+	pub subscript: Option<String>,
+	pub superscript: Option<String>,
+	pub bg: String,
+}
+
+impl Default for VisualText {
+	fn default() -> Self {
+		Self {
+			text: String::new(),
+			subscript: None,
+			superscript: None,
+			bg: String::from("none"),
+		}
+	}
+}
+
+impl VisualText {
+	pub fn plain_text(text: &str) -> Self {
+		VisualText {
+			text: text.to_string(),
+			..Default::default()
+		}
+	}
+
+	pub fn with_subscript(self, subscript: &str) -> Self {
+		Self {
+			subscript: Some(subscript.to_string()),
+			..self
+		}
+	}
+
+	pub fn with_superscript(self, superscript: &str) -> Self {
+		Self {
+			superscript: Some(superscript.to_string()),
+			..self
+		}
+	}
+}
+
+impl VisualText {
+	pub fn as_svg(&self, x: f64, y: f64, font_size: f64) -> String {
+		let mut res = format!(
+			"<text x='{x}' y='{y}' font-size='{font_size}' text-anchor='middle'>{}",
+			self.text
+		);
+
+		if let Some(subscript) = self.subscript.as_ref() {
+			res += &format!(
+				"<tspan font-size='{}'>{}</tspan>",
+				font_size * 0.4,
+				subscript
+			);
+		}
+
+		if let Some(superscript) = self.superscript.as_ref() {
+			res += &format!(
+				"<tspan font-size='{}' dx='-{}' dy='-{}'>{}</tspan>",
+				font_size * 0.4,
+				if self.subscript.is_some() {
+					font_size * 0.224
+				} else {
+					0.0
+				},
+				font_size * 0.5,
+				superscript
+			);
+		}
+
+		res += "</text>";
+
+		res
+	}
+}
+
+pub enum VisualRow {
+	String {
+		name: VisualText,
+		letters: Vec<Option<VisualText>>,
+	},
+	Arrow,
+	Empty,
+}
+
+const FONT_MULTIPLIER: f64 = 0.9;
+
+pub fn draw_rows(rows: Vec<VisualRow>, size: ImageSize) -> SVGImage {
+	let len = rows
+		.iter()
+		.map(|r| {
+			if let VisualRow::String { letters, .. } = r {
+				letters.len()
+			} else {
+				0
+			}
+		})
+		.max()
+		.unwrap_or_default();
+
+	// need to fit 3+3*strings.len() squares in height
+	// need to fit 4 + len squares in width
+	let (width, height, square_size, padding_w, padding_h) = {
+		match size {
+			ImageSize::FixedWidth(width) => {
+				let square_size = width as f64 / (4 + len) as f64;
+				let height = square_size * (2 + rows.len()) as f64;
+				(
+					width,
+					height.round() as u32,
+					square_size,
+					square_size,
+					square_size,
+				)
+			}
+			ImageSize::FixedHeight(height) => {
+				let square_size = height as f64 / (2 + rows.len()) as f64;
+				let width = square_size * (4 + len) as f64;
+				(
+					width.round() as u32,
+					height,
+					square_size,
+					square_size,
+					square_size,
+				)
+			}
+			ImageSize::Fixed { width, height } => {
+				let max_width = width as f64 / (4 + len) as f64;
+				let max_height = height as f64 / (rows.len()) as f64;
+				let square_size = max_height.min(max_width);
+
+				let padding_w = (width as f64 - square_size * (4 + len) as f64) / 2.0;
+				let padding_h = (height as f64 - square_size * (2 + rows.len()) as f64) / 2.0;
+				(width, height, square_size, padding_w, padding_h)
+			}
+		}
+	};
+
+	let mut res =
+		format!("<svg width='{width}' height='{height}' xmlns='http://www.w3.org/2000/svg'>");
+
+	for (y, row) in rows.iter().enumerate() {
+		match row {
+			VisualRow::String { name, letters } => {
+				res += &name.as_svg(
+					padding_w + 0.5 * square_size,
+					square_size * (y as f64 - LETTER_Y_ADJUST) + padding_h,
+					square_size * FONT_MULTIPLIER,
+				);
+
+				for (x, letter) in letters.iter().enumerate() {
+					let letter = match letter {
+						Some(letter) => letter,
+						_ => {
+							continue;
+						}
+					};
+
+					res += &format!(
+						"<rect x='{}' y='{}' width='{square_size}' height={square_size} style='fill:{};stroke:black;stroke-width:{}' />",
+						padding_w + square_size * (x + 2) as f64,
+						square_size * y as f64 + padding_h - square_size,
+						letter.bg,
+						square_size * 0.05
+					);
+					res += &letter.as_svg(
+						padding_w + square_size * ((x + 2) as f64 + 0.5),
+						square_size * (y as f64 - LETTER_Y_ADJUST) + padding_h,
+						square_size * FONT_MULTIPLIER,
+					);
+				}
+			}
+			VisualRow::Arrow => {
+				let left = width as f64 / 2. + square_size / 2.;
+				let right = left + square_size;
+				let top = square_size * (y as f64 + LETTER_Y_ADJUST) + padding_h;
+				let bot = square_size * (y as f64 - LETTER_Y_ADJUST) + padding_h;
+				res += &format!(
+					"<path d='M{} {} L{} {} L{} {} Z' />",
+					left,
+					top,
+					right,
+					top,
+					(left + right) / 2.,
+					bot,
+				);
+			}
+			VisualRow::Empty => {}
+		}
+	}
+
+	res.push_str("</svg>");
+	res
+}
